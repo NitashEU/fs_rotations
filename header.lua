514bfec4b4f1e8452afdbc020ae5043475038b6f
NitashEU <nitashsy@gmail.com>
2025-02-12 04:57:40 +0100

    docs: remove test lines from readme [trivial]

--- header.lua
+++ header.lua
@@ -1,7 +1,7 @@
 if false then
     return {
         name = "FS Rotations",
-        version = "1.2.1",
+        version = "1.2.2",
         author = "FS",
         load = false
     }
@@ -18,7 +18,7 @@ require("entry/index")
 
 local plugin = {
     name = "FS Rotations",
-    version = "1.2.1",
+    version = "1.2.2",
     author = "FS",
     load = true
 }
