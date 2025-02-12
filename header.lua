3af9a88d8bb454c06a905febf87572023945fd4e
NitashEU <nitashsy@gmail.com>
2025-02-12 05:01:37 +0100

    fix: verify commit message hook [trivial]

--- header.lua
+++ header.lua
@@ -1,7 +1,7 @@
 if false then
     return {
         name = "FS Rotations",
-        version = "1.3.1",
+        version = "1.3.2",
         author = "FS",
         load = false
     }
@@ -18,7 +18,7 @@ require("entry/index")
 
 local plugin = {
     name = "FS Rotations",
-    version = "1.3.1",
+    version = "1.3.2",
     author = "FS",
     load = true
 }
