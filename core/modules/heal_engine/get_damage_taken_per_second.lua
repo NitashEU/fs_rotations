e822f2045e253d961b7d0ca1d8d9da6c46644f03
Ferhat Sayan <fs@nitash.dev>
2025-03-04 21:56:47 +0100

    chore: remove unnecessary files and documentation [no-ref]

--- core/modules/heal_engine/get_damage_taken_per_second.lua
+++
@@ -1,7 +0,0 @@
---- Heal Engine Damage Calculation
---- Re-exports the damage calculation function from the damage analyzer for backward compatibility
-
--- Re-export the function from the damage analyzer
-FS.modules.heal_engine.get_damage_taken_per_second = function(unit, last_x_seconds)
-    return FS.modules.heal_engine.damage_analyzer.get_damage_taken_per_second(unit, last_x_seconds)
-end
